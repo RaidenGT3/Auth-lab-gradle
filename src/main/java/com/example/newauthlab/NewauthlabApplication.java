b22f2e48bb07339962112ff2f53b5cec44e87a8c
2311022 <2311022@A30224.student.ncs.ac.jp>
2026-09-24 11:57:04 +0900

    indent

--- src/main/java/com/example/newauthlab/NewauthlabApplication.java
+++ src/main/java/com/example/newauthlab/NewauthlabApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NewauthlabApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(NewauthlabApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(NewauthlabApplication.class, args);
+	}
 }
